cca0d26669543a1937ef465b5df97090be9ff3d3
sh970901 <aaddss639@naver.com>
2022-09-30 01:07:26 +0900

    @EnableBatchProcessing

--- src/main/java/com/example/ll/batch_app/BatchAppApplication.java
+++ src/main/java/com/example/ll/batch_app/BatchAppApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//배치 프로그램을 돌림
 @EnableBatchProcessing
 public class BatchAppApplication {
 
