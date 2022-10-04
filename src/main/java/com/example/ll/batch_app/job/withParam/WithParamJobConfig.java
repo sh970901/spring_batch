8d611b11d541d5cba8c5013a32fbf91d643ca3e8
sh970901 <aaddss639@naver.com>
2022-10-04 14:43:00 +0900

    파라미터 전달, 파라미터를 Tasklet에서 받기 edit configurations에서 파라미터 추가 --spring.batch.job.names=withParamJob name=Jim age=26

--- src/main/java/com/example/ll/batch_app/job/withParam/WithParamJobConfig.java
+++ src/main/java/com/example/ll/batch_app/job/withParam/WithParamJobConfig.java
@@ -2,38 +2,50 @@ package com.example.ll.batch_app.job.withParam;
 
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
+@Slf4j
 public class WithParamJobConfig {
     private final JobBuilderFactory jobBuilderFactory;
 
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job withParamJob() {
+    public Job withParamJob(Step withParamStep1) {
         return jobBuilderFactory.get("withParamJob")
-                .start(withParamStep1())
+                .start(withParamStep1)
                 .build();
     }
 
-    private Step withParamStep1() {
+    @Bean
+    @JobScope
+    public Step withParamStep1(Tasklet withParamStep1Tasklet) {
         return stepBuilderFactory.get("withParamStep1")
-                .tasklet(withParamStep1Tasklet())
+                .tasklet(withParamStep1Tasklet)
                 .build();
     }
 
-    private Tasklet withParamStep1Tasklet() {
+    @Bean
+    @StepScope
+    public Tasklet withParamStep1Tasklet(
+            @Value("#{jobParameters['name']}") String name,
+            @Value("#{jobParameters['age']}") String age
+    ) {
         return (contribution, chunkContext) -> {
-            System.out.println("WithParam 테스클릿 1");
+            log.debug("name : {}, age : {}", name, age);
 
             return RepeatStatus.FINISHED;
         };
