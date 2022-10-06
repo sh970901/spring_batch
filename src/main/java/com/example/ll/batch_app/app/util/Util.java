707cdb64fdbd7ba512235b2e6e3db141d1880745
sh970901 <aaddss639@naver.com>
2022-10-06 10:10:44 +0900

    연월 계산 Util, TDD

---
+++ src/main/java/com/example/ll/batch_app/app/util/Util.java
@@ -0,0 +1,27 @@
+package com.example.ll.batch_app.app.util;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Util {
+    public static class date {
+
+        public static int getEndDayOf(int year, int month) {
+            String yearMonth = year + "-" + "%02d".formatted(month);
+
+            return getEndDayOf(yearMonth);
+        }
+
+        public static int getEndDayOf(String yearMonth) {
+            LocalDate convertedDate = LocalDate.parse(yearMonth + "-01", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            convertedDate = convertedDate.withDayOfMonth(
+                    convertedDate.getMonth().length(convertedDate.isLeapYear()));
+
+            return convertedDate.getDayOfMonth();
+        }
+        public static LocalDateTime parse(String pattern, String dateText) {
+            return LocalDateTime.parse(dateText, DateTimeFormatter.ofPattern(pattern));
+        }
+    }
+}
---
+++ src/test/java/com/example/ll/batch_app/UtilTests.java
@@ -0,0 +1,32 @@
+package com.example.ll.batch_app;
+
+import com.example.ll.batch_app.app.util.Util;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UtilTests {
+    @Test
+    @DisplayName("특정월에 마지막날을 계산")
+    void t1() {
+        int endDayOf__2022_09 = Util.date.getEndDayOf(2022, 9);
+
+        assertThat(endDayOf__2022_09).isEqualTo(30);
+
+        int endDayOf__2022_08 = Util.date.getEndDayOf(2022, 8);
+
+        assertThat(endDayOf__2022_08).isEqualTo(31);
+    }
+    @Test
+    @DisplayName("문자열로 LocalDateTime 객체 만들기")
+    void t2() {
+        LocalDateTime localDateTime1 = Util.date.parse("yyyy-MM-dd HH:mm:ss.SSSSSS", "2022-09-01 23:59:59.999999");
+        assertThat(localDateTime1.toString().length()).isEqualTo(26);
+
+        LocalDateTime localDateTime2 = Util.date.parse("yyyy-MM-dd HH:mm:ss", "2022-09-01 23:59:59");
+        assertThat(localDateTime2.toString().length()).isEqualTo(19);
+    }
+}
